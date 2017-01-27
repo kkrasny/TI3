3599c5523cb03f20c74f76a2af069369a9ceddf5
Kamila <krasny.kamila@gmail.com>
2017-01-27 02:11:15 +0100

    Wybrana pierwsza cecha, policzone prawdopodobieństwa dla par z Y, brakuje policzyć dobrze sumę(I(Zi, Zd+1 ; Y) i będzie gotowe

--- TI3.py
+++ TI3.py
@@ -116,12 +116,12 @@ Z.remove(Z1)
 
 mm = 0
 f = 0
+I_d = 0
 
 while f <= d:
 
     for p in Zi:
         mm = 0
-        print(p)
         for j in Z:
             l_zz.append(dict(dct))
             l_zzy.append(dict(dctZZ))
@@ -132,40 +132,41 @@ while f <= d:
                 x = str(p[n]) + str(i)
                 zzy = x + str(yy)
                 ky = x + str(yy)
-                n += 1
                 l_zz[mm][x] += 1           # wystąpienia par w slowniku, nowe "p_x", prawdopodobieństwa trójek potem
                 l_zzy[mm][ky] += 1
+                n += 1
             mm += 1                        # prawdopodobieństwo liczone już w obliczeniach do informacji
         l_l_zz.append(l_zz)    # lista list, listy odpowiadają "ważnym" cechom, słowniki w nich kolejno wszystkim cechom
         l_l_zzy.append(l_zzy)  # jak wyżej, ale już połączone z Y
         f += 1
-    print(l_l_zzy)
+        l_zz = []
     P_z = l_l_zz
+    print(l_zz)
     print(P_z)
-
     # ogarnąć trójki (poniżej) !
     mm = 0
-
+    Nn = 20
     for p in Zi:
+        v = 0;
         q = 0
         for i in Z:                                 # ogarnąć TRÓJKI
-            px000 = l_zzy[q]['000'] / N             # to jest p(x,y), że x1=0, x2 = 0, y=0
-            px001 = l_zzy[q]['001'] / N
-            px010 = l_zzy[q]['010'] / N
-            px011 = l_zzy[q]['011'] / N
-            px100 = l_zzy[q]['100'] / N
-            px101 = l_zzy[q]['101'] / N
-            px110 = l_zzy[q]['110'] / N
-            px111 = l_zzy[q]['111'] / N
-            print(q)
-            pzz00 = int(P_z[0][q]['00']) / N      # to nie może być po prostu P_Z[q], tylko musi być P_Z[q][cos]
+            px000 = l_zzy[q]['000'] / Nn             # to jest p(x,y), że x1=0, x2 = 0, y=0
+            px001 = l_zzy[q]['001'] / Nn
+            px010 = l_zzy[q]['010'] / Nn
+            px011 = l_zzy[q]['011'] / Nn
+            px100 = l_zzy[q]['100'] / Nn
+            px101 = l_zzy[q]['101'] / Nn
+            px110 = l_zzy[q]['110'] / Nn
+            px111 = l_zzy[q]['111'] / Nn
+            pzz00 = int(P_z[v][q]['00']) / Nn      # to nie może być po prostu P_Z[q], tylko musi być P_Z[q][cos]
             print(pzz00)
-            pzz01 = int(P_z[0][q]['01']) / N
+            pzz01 = int(P_z[v][q]['01']) / Nn
             print(pzz01)
-            pzz10 = int(P_z[0][q]['10']) / N
+            pzz10 = int(P_z[v][q]['10']) / Nn
             print(pzz10)
-            pzz11 = int(P_z[0][q]['11']) / N
+            pzz11 = int(P_z[v][q]['11']) / Nn
             print(pzz11)
+            print(P_z[v][q]['11'])
             py0 = 1 - P_y
             py1 = P_y
             if pzz01 == 0 or pzz00 == 0 or pzz10 == 0 or pzz11 == 0:
@@ -180,11 +181,12 @@ while f <= d:
                 x11y0 = px10 * log2(px10 / (pzz11 * py0))
                 x11y1 = px11 * log2(px11 / (pzz11 * py1))
                 Iz = x00y0 + x00y1 + x01y0 + x01y1 + x10y0 + x10y1 + x11y0 + x11y1
+                I_d += Iz
                 Izz.append(Iz)
                 q += 1
+        v += 1
 
     iz = max(Izz)
-    print(iz)
     iz_ind = Izz.index(iz)
     Zn = Z[iz_ind]     # to ma być wiersz, który "wybieram"
     Zi.append(Zn)
@@ -192,11 +194,14 @@ while f <= d:
     Zi_ind.append(ind)
 
 
-
-Zi.sort()
-print(Zi_ind)
+s = ''
+Zi_ind.sort()
+I_d_str = str(I_d)
+for i in Zi_ind:
+    s += str(i)
 print(Zi)
 plik_wyj = open('output.txt' , 'w')
-plik_wyj.write(Zi_ind)
+plik_wyj.write('indeksy wybranych cech w kolejności rosnącej: ' + s + '\n' +
+               'wartość informacji wspólnej dla wszystkich cech: ' + I_d_str)
 
 plik_wyj.close()
